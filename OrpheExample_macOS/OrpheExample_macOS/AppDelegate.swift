990ed84bd7e19cec6b6647f31515c279c101b262
Kyosuke Nakanishi <kyoro920@gmail.com>
2017-03-31 18:04:28 +0900

    Orphe.frameworkをsubmoduleにしてビルド出来るようにした

---
+++ OrpheExample_macOS/OrpheExample_macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OrpheExample_macOS
+//
+//  Created by no new folk studio Inc. on 2016/12/21.
+//  Copyright © 2016 no new folk studio Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+//    func applicationDidFinishLaunching(aNotification: NSNotification) {
+//        // Insert code here to initialize your application
+//    }
+//
+//    func applicationWillTerminate(aNotification: NSNotification) {
+//        // Insert code here to tear down your application
+//    }
+
+
+}
+
---
+++ OrpheExample_macOS/OrpheExample_macOS/ViewController.swift
@@ -0,0 +1,235 @@
+//
+//  ViewController.swift
+//  OrpheExample_macOS
+//
+//  Created by no new folk studio Inc. on 2016/12/21.
+//  Copyright © 2016 no new folk studio Inc. All rights reserved.
+//
+
+import Cocoa
+import Orphe
+
+class ViewController: NSViewController {
+    
+    @IBOutlet weak var tableView: NSTableView!
+    @IBOutlet weak var leftSensorLabel: NSTextField!
+    @IBOutlet weak var rightSensorLabel: NSTextField!
+    var rssiTimer: Timer?
+    
+    var leftGesture = ""
+    var rightGesture = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.target = self
+        tableView.allowsTypeSelect = false
+        
+        ORPManager.sharedInstance.delegate = self
+        ORPManager.sharedInstance.isEnableAutoReconnection = false
+        ORPManager.sharedInstance.startScan()
+        
+        rssiTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(ViewController.readRSSI), userInfo: nil, repeats: true)
+    }
+    
+    override var representedObject: Any? {
+        didSet {
+            
+        }
+    }
+    
+    func updateCellsState(){
+        for (index, orp) in ORPManager.sharedInstance.availableORPDataArray.enumerated(){
+            for (columnNum, _) in tableView.tableColumns.enumerated(){
+                if let cell = tableView.view(atColumn: columnNum, row: index, makeIfNecessary: true) as? NSTableCellView{
+                    if orp.state() == .connected{
+                        cell.textField?.textColor = NSColor.yellow
+                        cell.textField?.backgroundColor = NSColor.darkGray
+                    }
+                    else{
+                        cell.textField?.textColor = NSColor.black
+                        cell.textField?.backgroundColor = NSColor.white
+                    }
+                }
+            }
+            
+        }
+    }
+    
+    override func keyDown(with theEvent: NSEvent) {
+        super.keyDown(with: theEvent)
+        if let lightNum:UInt8 = UInt8(theEvent.characters!){
+            for orp in ORPManager.sharedInstance.connectedORPDataArray{
+                orp.triggerLight(lightNum: lightNum)
+            }
+        }
+    }
+    
+}
+
+extension  ViewController: NSTableViewDelegate{
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView?{
+        let cellIdentifier: String = "NameCell"
+        
+        if let cell = tableView.make(withIdentifier: cellIdentifier, owner: nil) as? NSTableCellView {
+            if tableColumn == tableView.tableColumns[0] {
+                cell.textField?.stringValue = ORPManager.sharedInstance.availableORPDataArray[row].name
+                cell.textField?.drawsBackground = true
+            }
+            else if tableColumn == tableView.tableColumns[1] {
+                cell.textField?.stringValue = "0"
+                cell.textField?.drawsBackground = true
+            }
+            return cell
+        }
+        return nil
+    }
+    
+    func tableViewSelectionDidChange(_ notification: Notification) {
+        if tableView.selectedRow != -1 {
+            let orp = ORPManager.sharedInstance.availableORPDataArray[tableView.selectedRow]
+            if orp.state() == .disconnected{
+                ORPManager.sharedInstance.connect(orphe: orp)
+            }
+            else{
+                ORPManager.sharedInstance.disconnect(orphe: orp)
+            }
+        }
+    }
+    
+}
+
+extension ViewController: NSTableViewDataSource {
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return ORPManager.sharedInstance.availableORPDataArray.count
+    }
+    
+    
+}
+
+extension  ViewController: ORPManagerDelegate{
+    
+    //MARK: - ORPManagerDelegate
+    func orpheDidUpdateBLEState(state:CBCentralManagerState){
+        PRINT("didUpdateBLEState", state)
+        switch state {
+        case .poweredOn:
+            ORPManager.sharedInstance.startScan()
+        default:
+            break
+        }
+    }
+    
+    func orpheDidUpdateRSSI(orphe:ORPData){
+        print("didUpdateRSSI", orphe.RSSI)
+        if let index = ORPManager.sharedInstance.availableORPDataArray.index(of: orphe){
+            if let cell = tableView.view(atColumn: 1, row: index, makeIfNecessary: false) as? NSTableCellView{
+                cell.textField?.stringValue = String(describing: orphe.RSSI)
+            }
+        }
+    }
+    
+    func orpheDidDiscoverOrphe(orphe:ORPData){
+        PRINT("didDiscoverOrphe")
+        tableView.reloadData()
+        updateCellsState()
+    }
+    
+    func orpheDidDisappearOrphe(orphe:ORPData){
+        PRINT("didDisappearOrphe")
+        tableView.reloadData()
+        updateCellsState()
+    }
+    
+    func orpheDidFailToConnect(orphe:ORPData){
+        PRINT("didFailToConnect")
+        tableView.reloadData()
+        updateCellsState()
+    }
+    
+    func orpheDidDisconnect(orphe:ORPData){
+        PRINT("didDisconnect")
+        tableView.reloadData()
+        updateCellsState()
+    }
+    
+    func orpheDidConnect(orphe:ORPData){
+        PRINT("didConnect")
+        tableView.reloadData()
+        updateCellsState()
+        
+        orphe.setScene(.sceneSDK)
+        orphe.setGestureSensitivity(.high)
+    }
+    
+    func orpheDidUpdateOrpheInfo(orphe:ORPData){
+        PRINT("didUpdateOrpheInfo")
+        for orp in ORPManager.sharedInstance.connectedORPDataArray {
+            if orp != orphe && orp.side == orphe.side{
+                orp.switchToOppositeSide()
+                PRINT("switch to opposite side")
+            }
+        }
+    }
+    
+    //MARK: - Others
+    func readRSSI(){
+        for orp in ORPManager.sharedInstance.connectedORPDataArray {
+            orp.readRSSI()
+        }
+    }
+    
+    func orpheDidUpdateSensorData(orphe: ORPData) {
+        let sideInfo:Int32 = Int32(orphe.side.rawValue)
+        var text = ""
+        let quat = orphe.getQuat()
+        for (i, q) in quat.enumerated() {
+            text += "Quat\(i): "+String(q) + "\n"
+        }
+        
+        let euler = orphe.getEuler()
+        for (i, e) in euler.enumerated() {
+            text += "Euler\(i): "+String(e) + "\n"
+        }
+        
+        let acc = orphe.getAcc()
+        for (i, a) in acc.enumerated() {
+            text += "Acc\(i): "+String(a) + "\n"
+        }
+        
+        let gyro = orphe.getGyro()
+        for (i, g) in gyro.enumerated() {
+            text +=  "Gyro\(i): "+String(g) + "\n"
+        }
+        
+        let mag = orphe.getMag()
+        text +=  "Mag: "+String(mag) + "\n"
+        
+        let shock = orphe.getShock()
+        text += "Shock: "+String(shock) + "\n"
+        
+        if sideInfo == 0 {
+            leftSensorLabel.stringValue = "LEFT\n\n" + text + "\n" + leftGesture
+        }
+        else{
+            rightSensorLabel.stringValue = "RIGHT\n\n" + text + "\n" + rightGesture
+        }
+    }
+    
+    func orpheDidCatchGestureEvent(gestureEvent:ORPGestureEventArgs, orphe:ORPData) {
+        let side = orphe.side
+        let kind = gestureEvent.getGestureKindString() as String
+        let power = gestureEvent.getPower()
+        if side == ORPSide.left {
+            leftGesture = "Gesture: " + kind + "\n"
+            leftGesture += "power: " + String(power)
+        }
+        else{
+            rightGesture = "Gesture: " + kind + "\n"
+            rightGesture += "power: " + String(power)
+        }
+    }
+}
