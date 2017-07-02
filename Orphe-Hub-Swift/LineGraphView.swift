e4ab8765adc05128b4e0cf62c0b41e61eab7bdcb
Kyosuke Nakanishi <kyoro920@gmail.com>
2017-07-02 21:15:58 +0900

    繰り返さなくて良い処理をinitでやるように修正

--- Orphe-Hub-Swift/LineGraphView.swift
+++ Orphe-Hub-Swift/LineGraphView.swift
@@ -17,6 +17,9 @@ class LineGraphView: NSView{
     var bufSize:Int = 256
     var lineColor:NSColor = .red
     
+    let centerLine = NSBezierPath()
+    let line = NSBezierPath()
+    
     override init(frame frameRect: NSRect){
         super.init(frame: frameRect)
         
@@ -38,34 +41,32 @@ class LineGraphView: NSView{
         dataArray = Array(repeating: 0, count: bufSize)
         xInterval = CGFloat(self.frame.width) / CGFloat(dataArray.count)
         yInterval = CGFloat(self.frame.height) / (valueMax-valueMin)
-    }
-    
-    func addValue(_ value:CGFloat){
-        dataArray.removeFirst()
-        dataArray.append(value)
-    }
-    
-    override func draw(_ dirtyRect: NSRect) {
-        super.draw(dirtyRect)
-        let centerLine = NSBezierPath()
+        
         centerLine.move(to: CGPoint(x: 0, y: CGFloat(self.frame.height) / 2))
         centerLine.line(to: NSPoint(x: self.frame.width, y:CGFloat(self.frame.height) / 2))
         NSColor.white.setStroke()
         centerLine.lineWidth = 1
+        
+        line.lineWidth = 2
+    }
+    
+    func addValue(_ value:CGFloat){
+        dataArray.removeFirst()
+        dataArray.append(value*yInterval + CGFloat(self.frame.height) / 2)
+    }
+    
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
         centerLine.stroke()
         
-        let line = NSBezierPath()
-        // 起点
+        line.removeAllPoints()
         line.move(to: CGPoint(x: 0, y: CGFloat(self.frame.height) / 2))
         for (index, data) in dataArray.enumerated() {
-            line.line(to: NSPoint(x: CGFloat(index) * xInterval, y: data*yInterval + CGFloat(self.frame.height) / 2))
+            line.line(to: NSPoint(x: CGFloat(index) * xInterval, y: data))
         }
         
         // 色の設定
         lineColor.setStroke()
-        // ライン幅
-        line.lineWidth = 2
-        // 描画
         line.stroke()
         
     }
